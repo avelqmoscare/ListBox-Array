45eac31cc39550f5887a99cb8fd800a7d7bc442d
avel moscare <avelqmoscare@gmail.com>
2018-09-18 10:18:26 +0800

    i lost one action event

---
+++ ListBoxArray/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmStrings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstHolidaysEnum = new System.Windows.Forms.ListBox();
+            this.lstHolidaysStruct = new System.Windows.Forms.ListBox();
+            this.btnShowStructResult = new System.Windows.Forms.Button();
+            this.btnShowEnumResult = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstHolidaysEnum
+            // 
+            this.lstHolidaysEnum.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.lstHolidaysEnum.FormattingEnabled = true;
+            this.lstHolidaysEnum.Items.AddRange(new object[] {
+            "New year",
+            "All Saints Day",
+            "Labor Day",
+            "ChristmasMarikina",
+            "Muntinlupa"});
+            this.lstHolidaysEnum.Location = new System.Drawing.Point(12, 24);
+            this.lstHolidaysEnum.Name = "lstHolidaysEnum";
+            this.lstHolidaysEnum.Size = new System.Drawing.Size(132, 95);
+            this.lstHolidaysEnum.TabIndex = 0;
+            this.lstHolidaysEnum.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // lstHolidaysStruct
+            // 
+            this.lstHolidaysStruct.FormattingEnabled = true;
+            this.lstHolidaysStruct.Items.AddRange(new object[] {
+            "New Year",
+            "All Saints Day",
+            "Labor Day",
+            "ChristmasMarikina",
+            "Muntinlupa"});
+            this.lstHolidaysStruct.Location = new System.Drawing.Point(150, 24);
+            this.lstHolidaysStruct.Name = "lstHolidaysStruct";
+            this.lstHolidaysStruct.Size = new System.Drawing.Size(122, 95);
+            this.lstHolidaysStruct.TabIndex = 1;
+            // 
+            // btnShowStructResult
+            // 
+            this.btnShowStructResult.Location = new System.Drawing.Point(150, 148);
+            this.btnShowStructResult.Name = "btnShowStructResult";
+            this.btnShowStructResult.Size = new System.Drawing.Size(122, 23);
+            this.btnShowStructResult.TabIndex = 2;
+            this.btnShowStructResult.Text = "Show Struct Result";
+            this.btnShowStructResult.UseVisualStyleBackColor = true;
+            this.btnShowStructResult.Click += new System.EventHandler(this.btnShowStructResult_Click_1);
+            // 
+            // btnShowEnumResult
+            // 
+            this.btnShowEnumResult.Location = new System.Drawing.Point(12, 148);
+            this.btnShowEnumResult.Name = "btnShowEnumResult";
+            this.btnShowEnumResult.Size = new System.Drawing.Size(132, 23);
+            this.btnShowEnumResult.TabIndex = 3;
+            this.btnShowEnumResult.Text = "Show Enum Result";
+            this.btnShowEnumResult.UseVisualStyleBackColor = true;
+            this.btnShowEnumResult.Click += new System.EventHandler(this.btnShowStructResult_Click);
+            // 
+            // frmStrings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnShowEnumResult);
+            this.Controls.Add(this.btnShowStructResult);
+            this.Controls.Add(this.lstHolidaysStruct);
+            this.Controls.Add(this.lstHolidaysEnum);
+            this.Name = "frmStrings";
+            this.Text = "frmStrings";
+            this.Load += new System.EventHandler(this.frmStrings_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstHolidaysEnum;
+        private System.Windows.Forms.ListBox lstHolidaysStruct;
+        private System.Windows.Forms.Button btnShowStructResult;
+        private System.Windows.Forms.Button btnShowEnumResult;
+    }
+}
+
---
+++ ListBoxArray/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmStrings : Form
+    {
+        public frmStrings()
+        {
+            InitializeComponent();
+        }
+
+        enum Holidays { NewYear, AllSaintsDay, LaborDay, Christmas, Muntinlupa };
+        struct HolidayStruct
+        {
+            public string Holiday;
+        };
+
+
+        private void btnShowEnumResult_Click(object sender, EventArgs e)
+        {
+            int NewYear = (int)Holidays.NewYear;
+            int AllSaintsDay = (int)Holidays.AllSaintsDay;
+            int LaborDay = (int)Holidays.LaborDay;
+            int Christmas = (int)Holidays.Christmas;
+            int Muntinlupa = (int)Holidays.Muntinlupa;
+
+
+            string[] ListofHolidays = new string[] {lstHolidaysEnum.Items[0].ToString(),
+                                                    lstHolidaysEnum.Items[1].ToString(),
+                                                    lstHolidaysEnum.Items[2].ToString(),
+                                                    lstHolidaysEnum.Items[3].ToString(),
+                                                    lstHolidaysEnum.Items[4].ToString()};
+
+            MessageBox.Show(ListofHolidays[NewYear]);
+            MessageBox.Show(ListofHolidays[AllSaintsDay]);
+            MessageBox.Show(ListofHolidays[LaborDay]);
+            MessageBox.Show(ListofHolidays[Christmas]);
+            MessageBox.Show(ListofHolidays[Muntinlupa]);
+
+        }
+
+
+
+
+
+        private void btnShowStructResult_Click(object sender, EventArgs e)
+        {
+            HolidayStruct NewYearStruct = new HolidayStruct();
+            HolidayStruct AllSaintsDayStruct = new HolidayStruct();
+            HolidayStruct LaborDayStruct = new HolidayStruct();
+            HolidayStruct ChristmasStruct = new HolidayStruct();
+            HolidayStruct MuntinlupaStruct= new HolidayStruct();
+
+
+            NewYearStruct.Holiday = lstHolidaysStruct.Items[0].ToString();
+            AllSaintsDayStruct.Holiday = lstHolidaysStruct.Items[1].ToString();
+            LaborDayStruct.Holiday = lstHolidaysStruct.Items[2].ToString();
+            ChristmasStruct.Holiday = lstHolidaysStruct.Items[3].ToString();
+            MuntinlupaStruct.Holiday = lstHolidaysStruct.Items[4].ToString();
+
+            string[] ListofHolidayStruct = new string[] { NewYearStruct.Holiday,
+                                                          AllSaintsDayStruct.Holiday,
+                                                          LaborDayStruct.Holiday,
+                                                          ChristmasStruct.Holiday,
+                                                          MuntinlupaStruct.Holiday};
+            MessageBox.Show(ListofHolidayStruct[0]);
+            MessageBox.Show(ListofHolidayStruct[1]);
+            MessageBox.Show(ListofHolidayStruct[2]);
+            MessageBox.Show(ListofHolidayStruct[3]);
+            MessageBox.Show(ListofHolidayStruct[4]);
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnShowStructResult_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmStrings_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
